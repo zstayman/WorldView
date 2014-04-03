ea997a0f56cca09a9b5a9bfc7c42573cc5509bd3
zstayman <z.stayman@gmail.com>
2014-04-03 15:17:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldView::Application.config.secret_key_base = '69f597eb5a8038f4a6ed975cfc621695013fe40f2981354f7db4938e7ef6c266b07fceb9b5256326748d910a1862565dcfa2bdd964965bd30fb41f03c6c07e55'
