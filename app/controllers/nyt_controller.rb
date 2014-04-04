38797bc88c7890cd95fc4cea903b8ada5676cf02
github_name <github_email>
2014-04-04 15:06:53 -0400

    map renders

--- app/controllers/nyt_controller.rb
+++ app/controllers/nyt_controller.rb
@@ -2,8 +2,15 @@ class NytController < ApplicationController
   def new_stories
     response = HTTParty.get("http://api.nytimes.com/svc/news/v3/content/all/world;u.s.;business;washington;homepage;international+home;national;science;technology/1.json?api-key=#{NYT_WIRE}")
     results = response["num_results"]
+    offset = 20
     response["results"].each do |article|
-      $r.xadd "articles", article, 172800
+      $r.xadd "articles", article.to_json, 172800
+    end
+    while offset < results
+      HTTParty.get("http://api.nytimes.com/svc/news/v3/content/all/world;u.s.;business;washington;homepage;international+home;national;science;technology/1.json?offset=#{offset}&api-key=#{NYT_WIRE}")["results"].each do |article|
+        $r.xadd "articles", article.to_json, 172800
+      end
+      offset += 20
     end
   end
 end
