1ca0ad62f8292953805d5fd7ff40d8895395bf07
github_name <github_email>
2014-04-08 20:46:18 -0400

    heroku redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,9 @@
-$redis = Redis.new
-$r = Redis::ExpiringSet.new($redis)
+if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    $r = Redis::ExpiringSet.new($redis)
+  else
+    $redis = Redis.new
+    $r = Redis::ExpiringSet.new($redis)
+end
+
