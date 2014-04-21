51e3b5f15b20cf2fd19f65174c0837e09d55c0ff
github_name <github_email>
2014-04-21 12:08:59 -0400

    fixed?

--- app/assets/javascripts/models/article.js
+++ app/assets/javascripts/models/article.js
@@ -9,6 +9,7 @@ pin;
 // set counters and holders
 WorldView.geoJson = [];
 WorldView.placesCounter = 0;
+WorldView.map;
 
 // Article Model
 var Article = Backbone.Model.extend({
@@ -25,9 +26,9 @@ changeLatLng: function(){
   WorldView.geoJson.push(this.dropPin(this));
     // it takes too long to async, I need to call this later in the process
     // console.log("Pin Created")
-    if (WorldView.geoJson.length > WorldView.numberOfPlaces - 15) {
-      view.pushIn(map, WorldView.geoJson);
-    }
+    // if (WorldView.geoJson.length > WorldView.numberOfPlaces - 15) {
+    //   view.pushIn(map, WorldView.geoJson);
+    // }
     //debugger;
   },
 
